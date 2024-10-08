ee0f6a2f8ae009d837f35f08b2c4ceeb10ff8c55
JayGoel07 <jaygoel7@gmail.com>
2024-10-08 23:18:06 +0530

    first commit

---
+++ React-Portfolio-master/src/resumeData.js
@@ -0,0 +1,99 @@
+let resumeData = {
+    "name": "Jay Goel",
+    "role": "",
+    "roleDescription": "I am a 3rd-year undergraduate at IIT Ropar, pursuing B.Tech in Electrical Engineering. I am passionate about machine learning and web development, exploring new technologies and applying them in real-world projects. My curiosity drives me to constantly learn and build innovative solutions.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/jay-goel-239697259/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/JayGoel07/JayGoel07",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"instagram",
+          "url" : "https://www.instagram.com/je_suis_jaygoel/", 
+          "className":"fa fa-instagram"
+        }
+        
+      ],
+    "education":[
+      {
+        "UniversityName":"Indian Institute of Technology (IIT), Ropar, India",
+        "specialization":"Electrical Engineering",
+        "MonthOfPassing":"Bachelors in Technology",
+        "YearOfPassing":"2026",
+        "Achievements" : "Currently studying in 5th sem with the CGPA of 8.9"
+      },
+      {
+        "UniversityName":"D.A.V. Noida, Uttar Pradesh, India",
+        "specialization":"Senior Secondary School of Education",
+        "MonthOfPassing":"April",
+        "YearOfPassing":"2022",
+        "Achievements":"Passed with good grades of 96%"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"District Administration, Rupnagar ",
+        "specialization":"Management (Internship) ",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"Jan 2024 - May 2024",
+        "Achievements":" It was a project from PES, Pehchaan Ek Safar, an NGO run by students of IIT Ropar, in collaboration with District Administration, Rupnagar to educate the underpriviliged students of district, Rupnagar. Our role was to provide assistance and monitoring the team activities. "
+      },
+      {
+        "CompanyName":"Research Internship",
+        "specialization":"IIT Roorkee",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"May 2024 - Present",
+        "Achievements":"Working on machine learning and deep learning expertise on mainly image classification and many more."
+      }
+    ],
+    "skillsDescription":" Efficient skills ",
+    "skills":[
+      {
+        "skillname":"Programming Languages :  C/C++, Python, CSS, HTML, Javascript, SQL"
+      },
+      {
+        "skillname":"Libraries/Frameworks: Pandas, Numpy, OpenCV, ReactJs, NodeJs, ExpressJs, Matplotlib"
+      },
+      {
+        "skillname":"Tools: VS Code, Canva, Figma, Adobe Photoshop, GitHub, Excel, Google Colab, Tinkercad, Jupyter Notebook, MySQL"
+      }, 
+      
+      {
+        "skillname":"Machine Learning"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Railway Management System",
+        "description":"SQL based system",
+        "imgurl":"images/portfolio/railway.png",
+        "url":"https://github.com/JayGoel07/Railway_Management_System.git"
+      },
+      {
+        "name":"Full Stack Website",
+        "description":"Web development Project",
+        "imgurl":"images/portfolio/yelpcamp.png",
+        "url":"https://github.com/JayGoel07/fullstack-website.git"
+      },
+      {
+        "name":"Sentiment Analysis",
+        "description":"Machine Learning",  
+        "imgurl":"images/portfolio/sentiment.jpg", 
+        "url":"https://github.com/JayGoel07/JayGoel07.git"
+      },
+      {
+        "name":"Classification",
+        "description":"Machine Learning",
+        "imgurl":"images/portfolio/classify.png",
+        "url":"https://github.com/JayGoel07/JayGoel07.git"
+      }
+    ]
+  }
+  
+  export default resumeData
